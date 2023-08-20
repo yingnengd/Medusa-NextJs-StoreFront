d412d240fa7a36b4154faecb3d8ebabe8974220c
yingnengd <80525738+yingnengd@users.noreply.github.com>
2023-08-20 18:12:30 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com"],
+    // domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com"],
+    domains: ["http://10.12.1.235:9001"],
   },
 };
